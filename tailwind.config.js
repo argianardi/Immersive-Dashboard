d91bd15e076e667a7d7e355a6f9a8acf69e18862
argianardi <argianardi1435@gmail.com>
2022-09-19 20:33:01 +0700

    custom color

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        biruAlta: "#1B345F",
+        orangeAlta: "F7731C",
+        putihAlta: "F0F4FD",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
