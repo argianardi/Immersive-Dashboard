f7f07394d503ebbcfa402caf9f33aab2129e3661
argianardi <argianardi1435@gmail.com>
2022-09-19 20:34:11 +0700

    tailwind testing

---
+++ components/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p className="font-bold">Footer Component</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <p className="font-bold">Navbar </p>
+    </div>
+  );
+};
+
+export default Navbar;
