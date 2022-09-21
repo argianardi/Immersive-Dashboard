65454111f6a6739c83191e10c66dc8c96b0d4dae
argianardi <argianardi1435@gmail.com>
2022-09-21 08:31:06 +0700

    add testing

---
+++ pages/testing.jsx
@@ -0,0 +1,12 @@
+import Modal from "../components/modal";
+import React from "react";
+
+const Testing = () => {
+  return (
+    <div className="bg-blue-700 h-screen">
+      <Modal />
+    </div>
+  );
+};
+
+export default Testing;
